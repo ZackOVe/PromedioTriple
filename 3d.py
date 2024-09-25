f7b01fc11ac116e4a69dd7ac8171d7113ae63e28
MexiLander <a.23308051281220@cbtis128.edu.mx>
2024-09-24 19:52:27 -0600

    Add files via upload

---
+++ 3d.py
@@ -0,0 +1,9 @@
+numero1= input("Introduzca el primer numero:") #Pide al usuario el primer numero
+num1=int(numero1)#Permite al usuario ingresar el numero deseado
+numero2= input("Introduzca el segundo numero:")#Pide al usuario el primer numero
+num2=int(numero2)#Permite al usuario ingresar el numero deseado
+numero3= input("Introduzca el tercer numero: ")#Pide al usuario el primer numero
+num3=int(numero3)#Permite al usuario ingresar el numero deseado
+
+promedio = num1 + num2 + num3 / 3 #Realiza la multiplicacion de los numeros
+print ("El resultado es: ", promedio) #Muestra el resultado de la multiplicacion
